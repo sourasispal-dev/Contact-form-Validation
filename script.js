e636dff8f4420de931ba60b2f5b3d303e3f30882
Sourasis Pal <sourasispal@gmail.com>
2023-04-13 13:49:01 +0530

    buttton position changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validateForm() {
     var name = document.forms["myform"]["name"];
-    var email = document.forms["myform"]["email"];
     var phone = document.forms["myform"]["phone"];
+    var email = document.forms["myform"]["email"];
     var message = document.forms["myform"]["message"];
   
   
@@ -11,37 +11,42 @@ function validateForm() {
       name.style.border = "2px solid red";
       return false;
     }
+
+    if (phone.value == "") {
+      window.alert("Please enter your phone number.");
+      phone.focus();
+      return false;
+    }
+
+    if (phone.value.length != 10) {
+      window.alert("Invalid Phone No.");
+      phone.focus();
+      return false;
+    }
   
     if (email.value == "") {
       window.alert("Please enter your email.");
       email.focus();
       return false;
     }
+
     if (email.value.indexOf("@") < 0) {
       window.alert("Please enter your valid E-mail address.");
       email.focus();
       return false;
     }
+
     if (email.value.indexOf(".") < 0) {
       window.alert("Please enter your valid E-mail address.");
       email.focus();
       return false;
     }
-    if (phone.value == "") {
-      window.alert("Please enter your phone number.");
-      phone.focus();
-      return false;
-    }
-    if (phone.value.length != 10) {
-      window.alert("Invalid Phone No.");
-      phone.focus();
-      return false;
-    }
+  
     if(message.value == ""){
       window.alert("Please enter your message.");
       phone.focus();
       return false;
     }
     return true;
-  }
   
+}
